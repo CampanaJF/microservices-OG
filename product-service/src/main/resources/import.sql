2cfb9461bf438532e3b8f284451188921e9d8eb8
CampanaJF <jucampana@alumno.unlam.edu.ar>
2024-01-29 09:05:58 -0300

    micrometer

--- product-service/src/main/resources/import.sql
+++ product-service/src/main/resources/import.sql
@@ -1,10 +1,9 @@
-insert ignore into products (name, price, creation_date) values
-('Panasonic', 800, NOW()),
-('Sony', 700, NOW()),
-('Apple', 1000, NOW()),
-('Sony Notebook', 1000, NOW()),
-('Hewlett Packard', 500, NOW()),
-('Bianchi', 600, NOW()),
-('Nike', 100, NOW()),
-('Adidas', 200, NOW()),
-('Reebok', 300, NOW());
+insert into products (name, price, creation_date) values ('Panasonic', 800, '2024-01-25');
+insert into products (name, price, creation_date) values ('Sony', 700, '2024-01-25');
+insert into products (name, price, creation_date) values ('Apple', 1000, '2024-01-25');
+insert into products (name, price, creation_date) values ('Sony Notebook', 1000, '2024-01-25');
+insert into products (name, price, creation_date) values ('Hewlett Packard', 500, '2024-01-25');
+insert into products (name, price, creation_date) values ('Bianchi', 600, '2024-01-25');
+insert into products (name, price, creation_date) values ('Nike', 100, '2024-01-25');
+insert into products (name, price, creation_date) values ('Adidas', 200, '2024-01-25');
+insert into products (name, price, creation_date) values ('Reebok', 300, '2024-01-25');
